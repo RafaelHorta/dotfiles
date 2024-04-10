57d16195f22016df8574612af40b7e02e1c15a4c
RafaelHorta <CSHLeafar2609@gmail.com>
2024-04-10 02:10:43 -0600

    Changes in polybar launcher and swallp

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -6,29 +6,8 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 #killall -q polybar
 
-file_wal="${HOME}/.cache/wal/colors"
-file_colors="${HOME}/.config/polybar/colors.ini"
-content_colors=""
-list_colors=()
+$HOME/.config/polybar/makercolors.sh
 
-if [[ -f "$file_wal" ]]; then
-   touch $file_colors
-
-   list_colors=($(sed -n '1,8p' $file_wal))
-   content_colors="\nbackground = ${list_colors[0]}\nforeground = ${list_colors[7]}\nprimary = ${list_colors[6]}\ndisabled = ${list_colors[4]}\nalert = #9C0000"
-else
-   content_colors="
-      \nbackground = #000000
-      \nforeground = #ffffff
-      \nprimary = #003399
-      \ndisabled = #9999FF
-      \nalert = #9C0000
-   "
-fi
-
-echo -e "[colors]$content_colors" > $file_colors
-
-# Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log
 polybar menuh 2>&1 | tee -a /tmp/polybar1.log & disown
 
---
+++ config/polybar/makercolors.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+wal_path="${HOME}/.cache/wal/colors"
+colors_path="${HOME}/.config/polybar/colors.ini"
+colors_list=()
+colors_text="\nbackground = #000000\nforeground = #ffffff\nprimary = #003399\ndisabled = #9999FF\nalert = #9C0000"
+
+if [[ -f "$wal_path" ]]; then
+   touch $colors_path
+
+   colors_list=($(sed -n '1,8p' $wal_path))
+   colors_text="\nbackground = ${colors_list[0]}\nforeground = ${colors_list[7]}\nprimary = ${colors_list[6]}\ndisabled = ${colors_list[4]}\nalert = #9C0000"
+fi
+
+echo -e "[colors]$colors_text" > $colors_path
+
